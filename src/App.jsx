6c2a91ad8401524a58d5822fc2c5281918d5efed
Varshadharani <varshadharani43@gmail.com>
2024-02-02 21:39:40 +0530

    pla

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
-import { useState } from "react";
-import Item from "./Item";
-
+import axios from "axios";
+import "./App.css";
 const App = () => {
-	const [data, setData] = useState([]);
+	const [empData, setEmpData] = useState([]);
+	const [showData, setShowData] = useState([]);
+	const [pageNo, setPageNo] = useState(1);
+
 	useEffect(() => {
-		setData([
-			{ date: "2022-09-01", views: 100, article: "Article 1" },
-
-			{ date: "2023-09-01", views: 100, article: "Article 1" },
-
-			{ date: "2023-09-02", views: 150, article: "Article 2" },
-
-			{ date: "2023-09-02", views: 120, article: "Article 3" },
-
-			{ date: "2020-09-03", views: 200, article: "Article 4" },
-		]);
+		const fetchData = async () => {
+			try {
+				const url =
+					"https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+				const res = await axios(url, {
+					method: "GET",
+				});
+				setEmpData(res.data);
+				setShowData(res.data.slice(0, 10));
+			} catch (err) {
+				alert("failed to fetch data");
+			}
+		};
+		fetchData();
 	}, []);
-	const sortByDate = () => {
-		const sorted = [...data].sort(
-			(a, b) => new Date(b.date) - new Date(a.date)
-		);
-		setData(sorted);
+	const prevPage = () => {
+		if (pageNo > 1) {
+			setPageNo((prev) => prev - 1);
+			let startIndex = (pageNo - 2) * 10;
+			let endIndex = startIndex + 10;
+			let showRecords = empData.slice(startIndex, endIndex);
+			setShowData(showRecords);
+		}
 	};
-	const sortByViews = () => {
-		const sorted = [...data].sort((a, b) => b.views - a.views);
-		setData(sorted);
+	const nextPage = () => {
+		let totalPages = Math.ceil(empData.length / 10);
+		if (pageNo < totalPages) {
+			setPageNo((prev) => prev + 1);
+			let startIndex = pageNo * 10;
+			let endIndex = startIndex + 10;
+			let showRecords = empData.slice(startIndex, endIndex);
+			setShowData(showRecords);
+		}
 	};
+
 	return (
-		<div>
-			<h1>Date and Views Table</h1>
-			<button onClick={sortByDate}>Sort by Date</button>
-			<button onClick={sortByViews}>Sort by Views</button>
+		<div className="container">
+			<h1>Employee Data Table</h1>
 			<table>
 				<thead>
 					<tr>
-						<th>Date</th>
-						<th>Views</th>
-						<th>Article</th>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Role</th>
 					</tr>
 				</thead>
 				<tbody>
-					{data.map((item, id) => (
-						<Item key={id} item={item} />
-					))}
+					{showData?.map((data, idx) => {
+						return (
+							<tr key={data.id}>
+								<td>{data.id}</td>
+								<td>{data.name}</td>
+								<td>{data.email}</td>
+								<td>{data.role}</td>
+							</tr>
+						);
+					})}
 				</tbody>
 			</table>
+			<div className="pagination">
+				<button onClick={prevPage}>Previous</button>
+				<p>{pageNo}</p>
+				<button onClick={nextPage}>Next</button>
+			</div>
 		</div>
 	);
 };
